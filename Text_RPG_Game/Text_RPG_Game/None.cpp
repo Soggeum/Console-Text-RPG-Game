9b2f3fea5127a1f6b14a78b6c10eb77859eb55a1
sowsky <qudrnjs98@naver.com>
2025-09-05 19:13:33 +0900

    워리어,None 직업 공격할때 몬스터 남은체력출력빠진거 추가

--- Text_RPG_Game/Text_RPG_Game/None.cpp
+++ Text_RPG_Game/Text_RPG_Game/None.cpp
@@ -9,6 +9,7 @@ void None::Attack(Monster* mon)
 {
     //1회공격
     cout << Character::GetInstance()->getName() << "이(가) " << mon->getName() << "을(를) 공격합니다!" << endl;
+    cout << "현재 몬스터 체력: " << mon->getHp() << endl;
 
     mon->setHp(mon->getHp()-attack);
 
--- Text_RPG_Game/Text_RPG_Game/Warrior.cpp
+++ Text_RPG_Game/Text_RPG_Game/Warrior.cpp
@@ -9,4 +9,6 @@ void Warrior::Attack(Monster* mon)
 {    
     cout << Character::GetInstance()->getName() << "이(가) " << mon->getName() << "을(를) 공격합니다!" << endl;
     mon->setHp(mon->getHp() - attack);
+    cout << "현재 몬스터 체력: " << mon->getHp() << endl;
+
 }
