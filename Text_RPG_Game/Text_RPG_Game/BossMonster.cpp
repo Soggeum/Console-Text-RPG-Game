89352d308299582c2b58fb202672a83cc6c6b243
PDY <pdy127001@gmail.com>
2025-09-05 17:09:43 +0900

    보스몬스터 수정

--- Text_RPG_Game/Text_RPG_Game/BossMonster.cpp
+++ Text_RPG_Game/Text_RPG_Game/BossMonster.cpp
@@ -2,13 +2,13 @@
 BossMonster::BossMonster(int level) {
 	hp = (level * (rand() % 11 + 20)) * 1.5;//소수점버림
 	attack = (level * (rand() % 6 + 5))*1.5;//소수점버림
-	name = "보스몬스터";
+	name = "드래곤";
 }
 void BossMonster::takeDamage(int damage) {
 	setHp(hp - damage);
 }
 void BossMonster::displayStatus() const {
-	cout << "\033[31m"<< name << "의 체력: " << hp << ", 공격력 : " << attack<< "\033[0m"<< endl;
+	cout << "\033[31m 보스몬스터 " << name << "의 체력: " << hp << ", 공격력 : " << attack << "\033[0m" << endl;//구형 cmd제외 빨간색
 }
 Item* BossMonster::dropItem() {
 	int r = rand() % 100;
